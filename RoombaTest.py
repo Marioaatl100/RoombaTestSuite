ec5e0cd2ec1fcd09eede77859627732be0234778
Mario <marioivan95@hotmail.com>
2022-10-08 00:06:15 -0500

    Solved syntax mistake

--- RoombaTest.py
+++ RoombaTest.py
@@ -6,13 +6,13 @@ print("Hola")
 def test_api_post():
     data = { "roomSize" : [5, 5], 
     "coords" : [1, 2], 
-    "patches" : [[1, 0], [2, 2], [2, 3] ], 
+    "patches" : [ [1, 0], [2, 2], [2, 3] ], 
     "instructions" : "NNESEESWNWW"}
 
-    resp = requests.post(url="http://localhost:8080/v1/cleaning-sessions", data=data)
+    resp = requests.post(url="http://localhost:8080/v1/cleaning-sessions", data=data, 'Content-Type: application/json')
     print(resp.json())
     data = resp.json()
-    assert (resp.status_code == 200), "Status code is not 201. Rather found : "\
+    assert (resp.status_code == 200), "Status code is not 200. Rather found : "\
         + str(resp.status_code)
 
     
