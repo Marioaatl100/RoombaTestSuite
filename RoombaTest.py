f7944d8a3f8f915d08956a0cbf7ceda39392a234
Mario <marioivan95@hotmail.com>
2022-10-07 23:48:30 -0500

    Change asserts

--- RoombaTest.py
+++ RoombaTest.py
@@ -9,10 +9,6 @@ def test_api_post():
     data = resp.json()
     assert (resp.status_code == 200), "Status code is not 201. Rather found : "\
         + str(resp.status_code)
-    assert data['name'] == "John", "User created with wrong name. \
-        Expected : John, but found : " + str(data['name'])
-    assert data['job'] == "QA", "User created with wrong job. \
-        Expected : QA, but found : " + str(data['name'])
 
     print(resp.json())
 
