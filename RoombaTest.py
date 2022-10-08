03c2d1709b6e09e397e90ae7d563c7228f8dc697
Mario <marioivan95@hotmail.com>
2022-10-07 23:58:46 -0500

    Syntax issues

--- RoombaTest.py
+++ RoombaTest.py
@@ -6,7 +6,7 @@ print("Hola")
 def test_api_post():
     data = { "roomSize" : [5, 5], 
     "coords" : [1, 2], 
-    "patches" : { [1, 0], [2, 2], [2, 3] }, 
+    "patches" : [[1, 0], [2, 2], [2, 3] ], 
     "instructions" : "NNESEESWNWW"}
 
     resp = requests.post(url=" http://localhost:8080/v1/cleaning-sessions", data=data)
