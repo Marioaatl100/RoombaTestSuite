a448e8ce376f78c87aafc1e4f42fd505ea01340d
Mario <marioivan95@hotmail.com>
2022-10-07 23:46:56 -0500

    Added cleaning sessions endpoint

--- RoombaTest.py
+++ RoombaTest.py
@@ -2,27 +2,19 @@ import requests
 
 print("Hola")
 
-def test_api_get():
-    resp = requests.get("https://reqres.in/api/users?page=2")
-    assert (resp.status_code == 200), "Status code is not 200. Rather found : " + str(resp.status_code)
-    for record in resp.json()['data']:
-        if record['id'] == 4:
-            assert record['first_name'] == "Eve1",\
-                "Data not matched! Expected : Eve, but found : " + str(record['first_name'])
-            assert record['last_name'] == "Holt",\
-                "Data not matched! Expected : Holt, but found : " + str(record['last_name'])
 
 def test_api_post():
-    data = {'name': 'John',
-            'job': 'QA'}
-    resp = requests.post(url="https://reqres.in/api/users", data=data)
+    data = { "roomSize" : [5, 5], "coords" : [1, 2], "patches" : [ [1, 0], [2, 2], [2, 3] ], "instructions" : "NNESEESWNWW"}
+    resp = requests.post(url=" http://localhost:8080/v1/cleaning-sessions", data=data)
     data = resp.json()
-    assert (resp.status_code == 201), "Status code is not 201. Rather found : "\
+    assert (resp.status_code == 200), "Status code is not 201. Rather found : "\
         + str(resp.status_code)
     assert data['name'] == "John", "User created with wrong name. \
         Expected : John, but found : " + str(data['name'])
     assert data['job'] == "QA", "User created with wrong job. \
         Expected : QA, but found : " + str(data['name'])
 
-test_api_get()
+    print(resp.json())
+
+
 test_api_post()
