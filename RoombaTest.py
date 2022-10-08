209618348124c36f229b009369d94dc18126a1c0
Mario <marioivan95@hotmail.com>
2022-10-08 01:18:13 -0500

    Added pytest

--- RoombaTest.py
+++ RoombaTest.py
@@ -1,21 +1,17 @@
 import requests
+import pytest
 
-print("Hola")
-
-
-def test_api_post():
+@pytest.fixture(scope='module')
+def test_happy_path():
     data = { "roomSize" : [5, 5], 
     "coords" : [1, 2], 
     "patches" : [ [1, 0], [2, 2], [2, 3] ], 
     "instructions" : "NNESEESWNWW"}
 
     resp = requests.post(url="http://localhost:8080/v1/cleaning-sessions", json=data)
-    print(resp.json())
     data = resp.json()
     assert (resp.status_code == 200), "Status code is not 200. Rather found : "\
         + str(resp.status_code)
 
     
-
-
-test_api_post()
+test_happy_path()
