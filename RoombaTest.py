460c08ea5fe8e89755abb83c360350e4f6b70e1e
Mario <marioivan95@hotmail.com>
2022-10-07 23:55:59 -0500

    Made data changes

--- RoombaTest.py
+++ RoombaTest.py
@@ -4,7 +4,11 @@ print("Hola")
 
 
 def test_api_post():
-    data = { "roomSize" : [5, 5], "coords" : [1, 2], "patches" : [ [1, 0], [2, 2], [2, 3] ], "instructions" : "NNESEESWNWW"}
+    data = { "roomSize" : [5, 5], 
+    "coords" : [1, 2], 
+    "patches" : { [1, 0], [2, 2], [2, 3] ]}, 
+    "instructions" : "NNESEESWNWW"}
+
     resp = requests.post(url=" http://localhost:8080/v1/cleaning-sessions", data=data)
     print(resp.json())
     data = resp.json()
