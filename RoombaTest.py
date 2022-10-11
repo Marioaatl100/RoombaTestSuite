4cfb73c796d8918f8c2c2219799f82cd6ac4d270
Mario <marioivan95@hotmail.com>
2022-10-10 20:17:11 -0500

    Added happy path test

--- RoombaTest.py
+++ RoombaTest.py
@@ -7,14 +7,26 @@ def test_happy_path():
     "coords" : [1, 2], 
     "patches" : [ [1, 0], [2, 2], [2, 3] ], 
     "instructions" : "NNESEESWNWW"}
+    flag = True
 
     resp = requests.post(url="http://localhost:8080/v1/cleaning-sessions", json=data)
     data = resp.json()
-    if resp.status_code == 200:
+
+    print(data.coords)
+    print(data.patches)
+    if resp.status_code != 200:
+        flag = False
+
+    if data.coords != [1,3]:
+        flag = False
+       
+    if data.patches != 1:
+        flag = False
+
+
+    if flag:
         assert True
-    else:
+    else 
         assert False
-
-
     
 #test_happy_path()
